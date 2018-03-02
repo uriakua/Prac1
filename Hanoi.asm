d9e68d09ca3a1bb729a45b11542a74c21efdae06
uriakua <uriakua@gmail.com>
2018-03-01 23:58:41 -0600

    added missing commentaries and stetic changes

--- Hanoi.asm
+++ Hanoi.asm
@@ -1,8 +1,8 @@
-####### Hanoi Towers ||| Made by Guillermo Roldan Gomez #######
+	####### Hanoi Towers ||| Made by Guillermo Roldan Gomez #######
 .data
 
 .text
-	#reserve memory starting on 10010000 as instrcuted on the file, this is where the disks will be loaded
+	# reserve memory starting on 10010000 as instrcuted on the file, this is where the disks will be loaded
 	addi $a0, $zero, 0x10010000	#first rod	A, origin
 	addi $a1, $zero, 0x10010020	#second rod	B, aux
 	addi $a2, $zero, 0x10010040	#third rod	C, destiny
@@ -12,21 +12,21 @@
 	beq $s0, 1, baseCase	# base case
 	add $t0, $s0, $zero	# this is used to fill the rod A
 	addi $t4, $zero, 1	# this is used as a flag to compensate for the lack of slti, this is used to alternate between moving A and B
-	addi $s1, $zero, 8
-	addi $a3, $zero, 1		
-	addi $s2, $zero, 4
+	addi $s1, $zero, 8	# saved tempo to increase or decrease memory
+	addi $a3, $zero, 1	# argument to decrease variables	
+	addi $s2, $zero, 4	# saved temp to increase or decrease level 
 		
 																										
 fillrod:	
-	#fill the first rod with n disk 			
+	# fill the first rod with n disk 			
 	sw $t0, 0($a0)			# store disk n, n-1, n-2, n-3, n... on first pole A
 	sub $t0, $t0, $a3		# n-1 reduce n to fill necessary discs
-	add $a0, $a0, $s2		#move to next space on memory
-	bne $t0, $zero, fillrod		#if n != 0 return to fillrod until all disks are stored on first rod A	 
+	add $a0, $a0, $s2		# move to next space on memory
+	bne $t0, $zero, fillrod		# if n != 0 return to fillrod until all disks are stored on first rod A	 
 	
 		
 saveStack:
-	#move discs from A to be using C as an aux
+	# move discs from A to be using C as an aux
 	sub $sp, $sp, $s1	# move to next location on sp
 	sw $ra, 0($sp)		# store ra in the first place of the stack
 	sw $s0, 4($sp)		# save n on stack
@@ -34,6 +34,7 @@ saveStack:
 				
 	
 moveBtoC:		
+	# move B to C
 	sub $s0, $s0, $a3	# number of discs -1 
 	add $t1, $a1, $zero 	# store old B in temp 1
 	add $a1, $a2, $zero	# move C to B
@@ -42,6 +43,7 @@ moveBtoC:
 	jal saveStack		#  saveStack
 
 changeFlag:		
+	# change flag to 1
 	add $t5, $t5, $t4	# flag indicator
 	add $t1, $a1, $zero	# Store old B on T1	
 	add $a1, $a2, $zero	# Move C to B	
@@ -61,13 +63,13 @@ printDiscs:
 	jr $ra			# jump to return address
 	
 resetFlag:
-	# this is used to move A to B
-	add $t5, $zero, $zero	# set flag to zero	a2a2a2a2a2a2a2a2a2a2a2a2a2a2*
+	# this is used to return the flag to 0
+	add $t5, $zero, $zero	# set flag to zero	
 	lw $s0, 4($sp)		# load number of discs
 	sub $s0, $s0, $a3	# number of discs -1
 	
-	#beq $s0, $zero, baseCase
 moveBtoA:	
+	# move B to A
 	add $t1, $a0, $zero	# save on temp1 the value of old A	
 	add $a0, $a1, $zero	# Fill A with B
 	add $a1, $t1, $zero 	# Fill B with old A	  
@@ -75,6 +77,7 @@ moveBtoA:
 	jal saveStack		# jump to saveStack
 	
 moveAtoB:	
+	# move A to B
 	add $t2, $a0, $zero	# save on temp 2 old A	 
 	add $a0, $a1, $zero	# move B to A	
 	add $a1, $t2, $zero	# move old A to B	
@@ -85,9 +88,10 @@ moveAtoB:
 	j exit
 	
 baseCase:
-	sw $s0, 0($a0)
-	sw $t4, 0($a0)
-	sw $s0, 0($a2)
-	j exit
+	# base case when n = 1
+	sw $s0, 0($a0)	# store on first space of rod A n
+	sw $t4, 0($a0)	# store on temp 4 the value in rod A
+	sw $s0, 0($a2)	# store in the first level of C 1
+	j exit		# return 0
 	
 exit:
